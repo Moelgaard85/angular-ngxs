4962860a568a12a4b472aff98251db4a98992deb
Bjarne <bgp@proreact.dk>
2018-05-24 13:13:45 +0200

    Tutorial example done

--- src/app/read/read.component.ts
+++ src/app/read/read.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Tutorial } from '../models/tutorial.model';
+import { Store, Select } from '@ngxs/store';
+import { RemoveTutorial } from '../actions/tutorial.actions';
+import { TutorialState } from '../state/tutorial.state';
 
 @Component({
   selector: 'app-read',
@@ -7,7 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadComponent implements OnInit {
 
-  constructor() { }
+  @Select(TutorialState.getTutorials) tutorials$: Observable<Tutorial[]>;
+  tutorials: Tutorial[] = [];
+  // tutorials$: Observable<Tutorial>;
+
+  constructor(private store: Store) {
+    // this.tutorials$ = this.store.select(state => state.tutorials.tutorials);
+    this.tutorials$.subscribe((response: Tutorial[]) => {
+      this.tutorials = response;
+    });
+  }
+
+  delTutorial(name) {
+    this.store.dispatch(new RemoveTutorial(name));
+  }
 
   ngOnInit() {
   }
